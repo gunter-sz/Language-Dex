78a44798dc6c358a7703a5fa81e7e7230c5dca02
Arthur Cosentino <arthurcose@gmail.com>
2025-02-18 11:04:07 -0500

    Fix duplicated values in android manifest

--- scripts/android-prebuild.ts
+++ scripts/android-prebuild.ts
@@ -1,16 +1,42 @@
 import fs from "node:fs";
 import packageMeta from "../package.json";
 
-const gradleBuildFile = "./android/app/build.gradle";
-let data = fs.readFileSync(gradleBuildFile, "utf8");
+function patchBuildGradle() {
+  const path = "./android/app/build.gradle";
+  let data = fs.readFileSync(path, "utf8");
 
-function patch(name: string, value: string) {
-  const start = data.indexOf(name);
-  const end = data.indexOf("\n", start);
-  data = data.slice(0, start) + name + " " + value + data.slice(end);
+  function patch(name: string, value: string) {
+    const start = data.indexOf(name);
+    const end = data.indexOf("\n", start);
+    data = data.slice(0, start) + name + " " + value + data.slice(end);
+  }
+
+  patch("versionCode", JSON.stringify(packageMeta.versionCode));
+  patch("versionName", JSON.stringify(packageMeta.version));
+
+  fs.writeFileSync(path, data);
 }
 
-patch("versionCode", JSON.stringify(packageMeta.versionCode));
-patch("versionName", JSON.stringify(packageMeta.version));
+function patchAndroidManifest() {
+  // strip metadata for com.google.android.gms.ads, since it seems to duplicate
+  const path = "./android/app/src/main/AndroidManifest.xml";
+  let data = fs.readFileSync(path, "utf8");
 
-fs.writeFileSync(gradleBuildFile, data);
+  while (true) {
+    const startIndex = data.indexOf(
+      '\n    <meta-data android:name="com.google.android.gms.ads'
+    );
+
+    if (startIndex == -1) {
+      break;
+    }
+
+    const endIndex = data.indexOf("\n", startIndex + 1);
+    data = data.slice(0, startIndex) + data.slice(endIndex);
+  }
+
+  fs.writeFileSync(path, data);
+}
+
+patchBuildGradle();
+patchAndroidManifest();
